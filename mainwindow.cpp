dc525db82c8179e3d217b13deb953eeb8e49c931
Lawson McCoy <lawson.mccoy00@gmail.com>
2020-02-23 13:48:13 -0800

    Git wants me to commit again

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,39 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->infoFrame->setHidden(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_textBrowser_anchorClicked(const QUrl &arg1)
+{
+    ui->mainFrame->setHidden(true);
+    ui->infoFrame->setHidden(false);
+}
+
+void MainWindow::on_textBrowser_destroyed()
+{
+
+}
+
+void MainWindow::on_infoFrame_destroyed(QObject *arg1)
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->mainFrame->setHidden(true);
+    ui->infoFrame->setHidden(false);
+}
+
+void MainWindow::on_back_clicked()
+{
+    ui->mainFrame->setHidden(false);
+    ui->infoFrame->setHidden(true);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_textBrowser_anchorClicked(const QUrl &arg1);
+
+    void on_textBrowser_destroyed();
+
+    void on_infoFrame_destroyed(QObject *arg1);
+
+    void on_pushButton_clicked();
+
+    void on_back_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
