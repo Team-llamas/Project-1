6af18b9bc91337c8c3862a96b9c88e54df995949
Lawson McCoy <lawson.mccoy00@gmail.com>
2020-03-23 13:21:13 -0700

    Finish the documentation for doxygen

--- brochure/interestlevel.h
+++ brochure/interestlevel.h
@@ -1,6 +1,13 @@
 #ifndef INTERESTLEVEL_H
 #define INTERESTLEVEL_H
 
+/** enum
+ * interestLeve
+ *      An enum stating the amout of interest a customer
+ *      has in the products being sold. Values in order
+ *      are NO_INTEREST, LOW_INTEREST, MODERATE_INTEREST,
+ *      HIGH_INTEREST, and EXTREMELY_HIGH_INTEREST
+ */
 enum interestLevel
 {
     NO_INTEREST,
